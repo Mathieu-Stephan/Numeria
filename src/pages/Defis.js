147957aa0273967a18f874e64bec46db58c7686c
NathanBasol <basol.nathan@gmail.com>
2024-04-18 14:21:28 +0200

    placeholder page defis

--- src/pages/Defis.js
+++ src/pages/Defis.js
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from './NavBar';
 import Footer from './Footer';
 
 const Defis = () => {
-    fetch('http://localhost:3001/api/data')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Erreur lors de la récupération des données', error));
+    const [defis, setDefis] = useState([]);
+
+    useEffect(() => {
+        // Données de défis fictives pour les tests
+        const defisData = [
+            { id: 1, titre: "Défi 1", description: "Description du Défi 1", etoiles: 4 },
+            { id: 2, titre: "Défi 2", description: "Description du Défi 2", etoiles: 3 },
+            { id: 3, titre: "Défi 3", description: "Description du Défi 3", etoiles: 5 },
+            { id: 4, titre: "Défi 4", description: "Description du Défi 4", etoiles: 2 },
+            { id: 5, titre: "Défi 5", description: "Description du Défi 5", etoiles: 4 },
+            { id: 6, titre: "Défi 6", description: "Description du Défi 6", etoiles: 3 }
+        ];
+
+        // Mise à jour de l'état avec les données de défis fictives
+        setDefis(defisData);
+    }, []);
 
     return (
         <div className="container">
             <Navbar />
+            <div className="content">
+                <h1>Liste des Défis</h1>
+            </div>
+            <div className="defis-container">
+                {/* Affichage des défis */}
+                {defis.map((defi, index) => (
+                    <div key={defi.id} className="defi-square">
+                        <div className="defi-card">
+                            <h2>{defi.titre}</h2>
+                            <p>{defi.description}</p>
+                            {/* Affichage du nombre d'étoiles */}
+                            <div>
+                                {[...Array(defi.etoiles)].map((star, index) => (
+                                    <span key={index} className="star">&#9733;</span>
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
             <Footer />
         </div>
     );
